d67e98a2a18b5972d4e254c5038e250bbdd2e39b
Salahuddin <salahuddin31032006@gmail.com>
2025-02-23 21:53:21 +0530

    i created sub class of class and how to pass the parameter to the sub class

--- main.py
+++ main.py
@@ -15,6 +15,25 @@ class Animal:
     def walk(self):
         print(f'{self.name} is walking')
 
+    def run(self):
+        print(f'{self.name} is running')
+
+class Dog(Animal):
+    def __init__(self,name,age,breed):
+        super().__init__(name, age,"Dog")
+        self.breed = breed
+
+    def bark(self):
+        print(f'{self.name} is barking')
+    
+# Now we create object of the class dog
+golden_retreiver = Dog('oreo' , 5 , 'goldenretreiver')
+
+golden_retreiver.bark()
+
+golden_retreiver.walk()
+        
+
 # create an object for a human
 human = Animal('aman' , '20' , 'Human')
 
@@ -24,4 +43,7 @@ human.speak()
 # make your human walk
 human.walk()
 
+
+
 #Hw create a class vehicle and take the attributes from the parameter in the constructor and add the relevent methods to it
+# Hw create 3 child classes(or sub class ) of animal class and create object of each child class(or sub class )
